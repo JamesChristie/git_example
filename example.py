e3d6318ca92a87cad0e98687a971e37c7f758d44
James Christie <jchristie@8thlight.com>
2016-12-09 03:11:00 -0600

    Add new methods to Python file

--- example.py
+++ example.py
@@ -1,6 +1,12 @@
 class SomeExampleClass:
-  def __init__():
+  def __init__(self):
     self.some_value = "A string of text"
 
-  def an_instance_method():
+  def an_instance_method(self):
     return self.some_value
+
+  def an_instance_method_with_prefix(self):
+    return "A prefix: " + self.some_value
+
+  def do_addition(self):
+    return 7 + 13
